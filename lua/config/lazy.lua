b079cba77980cf82c5ae196f6fc67220dfbfe320
sc0rp10n16 <akshithmysa.physics@gmail.com>
2024-12-04 21:14:43 +0530

    added LazyExtras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,9 +17,31 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = {
         -- add LazyVim and import its plugins
-        { "LazyVim/LazyVim", import = "lazyvim.plugins", opt = {
-            colorscheme = "solarized-osaka",
-        } },
+        {
+            "LazyVim/LazyVim",
+            import = "lazyvim.plugins",
+            opt = {
+                colorscheme = "solarized-osaka",
+            },
+        },
+        -- import any extras modules here
+        { import = "lazyvim.plugins.extras.linting.eslint" },
+        { import = "lazyvim.plugins.extras.formatting.prettier" },
+        { import = "lazyvim.plugins.extras.lang.typescript" },
+        { import = "lazyvim.plugins.extras.lang.json" },
+        { import = "lazyvim.plugins.extras.lang.python" },
+        { import = "lazyvim.plugins.extras.lang.rust" },
+        { import = "lazyvim.plugins.extras.lang.tailwind" },
+        { import = "lazyvim.plugins.extras.lang.markdown" },
+        { import = "lazyvim.plugins.extras.lang.toml" },
+        { import = "lazyvim.plugins.extras.lang.docker" },
+        { import = "lazyvim.plugins.extras.lang.git" },
+        { import = "lazyvim.plugins.extras.lang.go" },
+        { import = "lazyvim.plugins.extras.lang.prisma" },
+        { import = "lazyvim.plugins.extras.lang.sql" },
+        { import = "lazyvim.plugins.extras.lang.yaml" },
+        { import = "lazyvim.plugins.extras.coding.yanky" },
+        { import = "lazyvim.plugins.extras.ai.copilot" },
         -- import/override with your plugins
         { import = "plugins" },
     },
